17f75cca1b096bda00d5cc088be450a34eeee06e
proehr <pauline.roehr@yahoo.de>
2024-01-09 12:49:08 +0100

    Fix initial formatting

--- html/src/com/pli/codes/architectureacrobat/client/HtmlLauncher.java
+++ html/src/com/pli/codes/architectureacrobat/client/HtmlLauncher.java
@@ -7,16 +7,16 @@ import com.pli.codes.architectureacrobat.GameApplication;
 
 public class HtmlLauncher extends GwtApplication {
 
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                // Resizable application, uses available space in browser
-                return new GwtApplicationConfiguration(true);
-                // Fixed size application:
-                //return new GwtApplicationConfiguration(480, 320);
-        }
+    @Override
+    public GwtApplicationConfiguration getConfig() {
+        // Resizable application, uses available space in browser
+        return new GwtApplicationConfiguration(true);
+        // Fixed size application:
+        //return new GwtApplicationConfiguration(480, 320);
+    }
 
-        @Override
-        public ApplicationListener createApplicationListener () {
-                return new GameApplication();
-        }
+    @Override
+    public ApplicationListener createApplicationListener() {
+        return new GameApplication();
+    }
 }
