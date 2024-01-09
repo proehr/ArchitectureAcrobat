125e12d18f153169fcdb84aadd128c1ced3dd669
proehr <pauline.roehr@yahoo.de>
2024-01-09 12:46:49 +0100

    Initial commit

---
+++ desktop/src/com/pli/codes/architectureacrobat/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.pli.codes.architectureacrobat;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.pli.codes.architectureacrobat.GameApplication;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Architecture Acrobat");
+		new Lwjgl3Application(new GameApplication(), config);
+	}
+}
