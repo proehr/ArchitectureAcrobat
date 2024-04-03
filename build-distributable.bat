79b1ee1f48bfc43bc08d5a4eb51646e483965127
proehr <pauline.roehr@yahoo.de>
2024-04-03 12:04:19 +0200

    Update build script for exe

--- build-distributable.bat
+++ build-distributable.bat
@@ -1,6 +1,6 @@
 SET JAVA_HOME=C:\Users\herec\.jdks\azul-17.0.6
-RMDIR /S /Q "desktop\build\"
-CALL gradlew clean desktop:dist
+RMDIR /S /Q "lwjgl3\build\"
+CALL gradlew clean lwjgl3:dist
 jpackage --type app-image --name "Architecture Acrobat" ^
-    --input desktop\build\libs --main-jar desktop-1.0.jar ^
-    --dest desktop\build
+    --input lwjgl3\build\lib --main-jar ArchitectureAcrobat-1.0.0.jar ^
+    --dest lwjgl3\build
